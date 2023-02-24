90bd81f28dbfb6dfffb4e70927bac7b23c545b0f
Lucas Schmidt <lucas.schmidt015@gmail.com>
2023-02-23 22:38:21 -0300

    Implemented a header component and dropdown funtions.

--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,7 +1,38 @@
+import Img from '../../Resources/MinhaFoto.png';
+import { CSSTransition } from 'react-transition-group';
+import './Header.css'
+import { ImExit } from 'react-icons/im'
+import { AiFillFileUnknown } from 'react-icons/ai'
+import { useState } from 'react';
+
 export default function Header(){
+
+    const [showBackdown, setShowBackdown] = useState(false);
+
+    // function showBackdown(){
+        
+    // }
+
     return(
-        <div>
-            <h1>Header</h1>
+        <div className="header-container">
+            <h2>SpotTunes</h2>
+            <div className="profile-options">
+                <span>User Name</span>
+                    
+                <img src={Img} alt="UserName" onClick={() => setShowBackdown(!showBackdown)}/>
+                <CSSTransition in={showBackdown} classNames="component" timeout={200} unmountOnExit>
+                    <div className='dropdown' id='dropdown'>
+                        <div>
+                            <AiFillFileUnknown/>
+                            <p>About</p>
+                        </div>
+                        <div>
+                            <ImExit/>
+                            <p>Exit</p>
+                        </div>
+                    </div>                
+                </CSSTransition>
+            </div>
         </div>
     );
 }
